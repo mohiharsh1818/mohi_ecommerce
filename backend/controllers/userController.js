8fb63881c18890fffabfe27474c0f9872f83d139
mohiharsh1818 <mohithraaj2004@gmail.com>
2025-06-12 21:45:09 +0530

    strong password added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -54,7 +54,7 @@ const registerUser = async (req, res) => {
         if (!validator.isEmail(email)) {
             return res.json({ success: false, message: "Please enter a valid email" })
         }
-        if (password.length < 8) {
+        if (!validator.isStrongPassword(password)) {
             return res.json({ success: false, message: "Please enter a strong password" })
         }
 
